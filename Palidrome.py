3d33538bfb58b797f92815ed7b61695152cf1b5e
moorthy934480-svg <moorthy934480@gmail.com>
2025-08-17 11:55:26 +0530

    Create Palidrome.py

---
+++ Palidrome.py
@@ -0,0 +1,9 @@
+text = input("Enter a word: ")
+if text == text[::-1]:
+    print("It's a palindrome!")
+else:
+    print("It's not a palindrome.")
+
+*Example Output:*
+Enter a word: madam  
+It's a palindrome!
