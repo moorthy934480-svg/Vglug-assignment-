87e7843af823bd224022f80e4aad97aa73049037
moorthy934480-svg <moorthy934480@gmail.com>
2025-08-17 12:07:36 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,26 @@
+A="Thirumoorthi"
+print(len(A))
+print(A.upper())
+print(A.lower())
+print(A.capitalize())
+print(A.title())
+print(A.isalnum())
+# print(A.sascii()) # This is not a sta
+print(A.isdecimal())
+print(A.isdigit())
+print(A.islower())
+print(A.isupper())
+print(A.isspace())
+
+output:
+12
+THIRUMOORTHI
+thirumoorthi
+Thirumoorthi
+Thirumoorthi
+True
+False
+False
+False
+False
+False
